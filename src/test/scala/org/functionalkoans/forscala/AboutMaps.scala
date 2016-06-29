3d2050404d6943947b353c60cbf437636332aec0
Meredith Godar <meredithgodar@iseinc.biz>
2016-06-29 13:05:10 -0500

    Completed AboutMaps

--- src/test/scala/org/functionalkoans/forscala/AboutMaps.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutMaps.scala
@@ -6,12 +6,12 @@ class AboutMaps extends KoanSuite {
 
   koan("Maps can be created easily") {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
-    myMap.size should be(__)
+    myMap.size should be(4)
   }
 
   koan("Maps contain distinct pairings") {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "MI" -> "Michigan")
-    myMap.size should be(__)
+    myMap.size should be(3)
   }
 
   koan("Maps can be added to easily") {
@@ -19,8 +19,7 @@ class AboutMaps extends KoanSuite {
 
     val aNewMap = myMap + ("IL" -> "Illinois")
 
-    aNewMap.contains("IL") should be(__)
-
+    aNewMap.contains("IL") should be(true)
   }
 
   koan("Map values can be iterated") {
@@ -28,12 +27,12 @@ class AboutMaps extends KoanSuite {
 
     val mapValues = myMap.values
 
-    mapValues.size should be(__)
+    mapValues.size should be(3)
 
-    mapValues.head should be(__)
+    mapValues.head should be("Michigan")
 
     val lastElement = mapValues.last
-    lastElement should be(__)
+    lastElement should be("Wisconsin")
 
     // for (mval <- mapValues) println(mval)
 
@@ -46,15 +45,15 @@ class AboutMaps extends KoanSuite {
 
     val mapValues = myMap.values
 
-    mapValues.size should be(__)
+    mapValues.size should be(3)
 
-    myMap("MI") should be(__)
+    myMap("MI") should be("Meechigan")
   }
 
   koan("Map keys may be of mixed type") {
     val myMap = Map("Ann Arbor" -> "MI", 49931 -> "MI")
-    myMap("Ann Arbor") should be(__)
-    myMap(49931) should be(__)
+    myMap("Ann Arbor") should be("MI")
+    myMap(49931) should be("MI")
   }
 
   koan("Mixed type values can be added to a map ") {
@@ -62,66 +61,62 @@ class AboutMaps extends KoanSuite {
     myMap("Ann Arbor") = (48103, 48104, 48108)
     myMap("Houghton") = 49931
 
-    myMap("Houghton") should be(__)
-    myMap("Ann Arbor") should be((__, __, __))
+    myMap("Houghton") should be(49931)
+    myMap("Ann Arbor") should be((48103, 48104, 48108))
     // what happens if you change the Any to Int
   }
 
-
   koan("Maps may be accessed") {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
-    myMap("MI") should be(__)
-    myMap("IA") should be(__)
+    myMap("MI") should be("Michigan")
+    myMap("IA") should be("Iowa")
   }
 
   koan("Map elements can be removed easily") {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
     val aNewMap = myMap - "MI"
-    aNewMap.contains("MI") should be(__)
+    aNewMap.contains("MI") should be(false)
   }
 
   koan("Accessing a map by key results in an exception if key is not found") {
-
     val myMap = Map("OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
 
-    // Cheat Code (because this is hard to illustrate): uncomment the intercept code to make this pass 
-    //intercept[NoSuchElementException] {
-
+    // Cheat Code (because this is hard to illustrate): uncomment the intercept code to make this pass
+    intercept[NoSuchElementException] {
       myMap("MI") should be(__)
-    //}
+    }
   }
 
   koan("Map elements can be removed in multiple") {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
     val aNewMap = myMap -- List("MI", "OH")
 
-    aNewMap.contains("MI") should be(__)
+    aNewMap.contains("MI") should be(false)
 
-    aNewMap.contains("WI") should be(__)
-    aNewMap.size should be(__)
+    aNewMap.contains("WI") should be(true)
+    aNewMap.size should be(2)
   }
 
   koan("Map elements can be removed with a tuple") {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
     val aNewMap = myMap - ("MI", "WI") // Notice: single '-' operator for tuples
 
-    aNewMap.contains("MI") should be(__)
-    aNewMap.contains("OH") should be(__)
-    aNewMap.size should be(__)
+    aNewMap.contains("MI") should be(false)
+    aNewMap.contains("OH") should be(true)
+    aNewMap.size should be(2)
   }
 
   koan("Attempted removal of nonexistent elements from a map is handled gracefully") {
     val myMap = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
     val aNewMap = myMap - "MN"
 
-    aNewMap.equals(myMap) should be(__)
+    aNewMap.equals(myMap) should be(true)
   }
 
   koan("Map equivalency is independent of order") {
-
     val myMap1 = Map("MI" -> "Michigan", "OH" -> "Ohio", "WI" -> "Wisconsin", "IA" -> "Iowa")
     val myMap2 = Map("WI" -> "Wisconsin", "MI" -> "Michigan", "IA" -> "Iowa", "OH" -> "Ohio")
 
-    myMap1.equals(myMap2) should be(__)
+    myMap1.equals(myMap2) should be(true)
   }
 }
