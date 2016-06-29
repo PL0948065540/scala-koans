99aa20bab41bc7ce143a0eddca497b946634d94e
Meredith Godar <meredith.godar@gmail.com>
2016-06-29 00:09:08 -0500

    Completed AboutParentClasses

--- src/test/scala/org/functionalkoans/forscala/AboutParentClasses.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutParentClasses.scala
@@ -8,8 +8,8 @@ class AboutParentClasses extends KoanSuite {
     class Employee(override val firstName: String, override val lastName: String,
                    val employeeID: Long) extends Worker(firstName, lastName)
     val me = new Employee("Name", "Yourself", 1233)
-    me.firstName should be(__)
-    me.lastName should be(__)
+    me.firstName should be("Name")
+    me.lastName should be("Yourself")
   }
 
   koan("A class that extends from another is polymorphic") {
@@ -20,8 +20,8 @@ class AboutParentClasses extends KoanSuite {
     val me = new Employee("Name", "Yourself", 1233)
     val worker: Worker = me
 
-    worker.firstName should be(__)
-    worker.lastName should be(__)
+    worker.firstName should be("Name")
+    worker.lastName should be("Yourself")
   }
 
   koan("An abstract class, as in Java, cannot be instantiated and only inherited") {
@@ -30,7 +30,6 @@ class AboutParentClasses extends KoanSuite {
     // if you uncomment this line, if will fail compilation
     //val worker = new Worker
   }
-
 
   koan("A class can be placed inside an abstract class just like in java") {
     abstract class Worker(val firstName: String, val lastName: String) {
@@ -43,6 +42,6 @@ class AboutParentClasses extends KoanSuite {
 
     val employee = new Employee("Name", "Yourself", 2291)
     val assignment = new employee.Assignment(22)  //using the employee instance's path, create an assignment for it.
-    assignment.hours should be (__)
+    assignment.hours should be (22)
   }
 }
