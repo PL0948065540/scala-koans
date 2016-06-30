9931687e60ff3416aab5594bb7f428599f7e6799
Meredith Godar <meredith.godar@gmail.com>
2016-06-29 19:45:27 -0500

    Completed AboutPartiallyAppliedFunctions

--- src/test/scala/org/functionalkoans/forscala/AboutPartiallyAppliedFunctions.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutPartiallyAppliedFunctions.scala
@@ -9,14 +9,14 @@ class AboutPartiallyAppliedFunctions extends KoanSuite with ShouldMatchers {
          | doesn't apply any arguments""") {
     def sum(a: Int, b: Int, c: Int) = a + b + c
     val sum3 = sum _
-    sum3(1, 9, 7) should be(__)
-    sum(4, 5, 6) should be(__)
+    sum3(1, 9, 7) should be(1 + 9 + 7)
+    sum(4, 5, 6) should be(4 + 5 + 6)
   }
 
   koan("""Partially applied functions can replace any number of arguments""") {
     def sum(a: Int, b: Int, c: Int) = a + b + c
     val sumC = sum(1, 10, _: Int)
-    sumC(4) should be(__)
-    sum(4, 5, 6) should be(__)
+    sumC(4) should be(1 + 10 + 4)
+    sum(4, 5, 6) should be(4 + 5 + 6)
   }
 }
