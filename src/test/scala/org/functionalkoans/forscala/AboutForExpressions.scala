181b4a8086efa43048cc3e58af713c87c3b1bbb9
Meredith Godar <meredith.godar@gmail.com>
2016-06-29 20:17:47 -0500

    Completed AboutForExpressions

--- src/test/scala/org/functionalkoans/forscala/AboutForExpressions.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutForExpressions.scala
@@ -10,7 +10,7 @@ class AboutForExpressions extends KoanSuite {
     for (i <- someNumbers)
       sum += i
 
-    sum should equal(__)
+    sum should equal(0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9)
   }
 
   koan("For loops can contain additional logic") {
@@ -20,15 +20,15 @@ class AboutForExpressions extends KoanSuite {
     for (i <- someNumbers)
       if (i % 2 == 0) sum += i
 
-    sum should equal(__)
+    sum should equal(0 + 2 + 4 + 6 + 8)
   }
-  
+
   koan("For expressions can nest, with later generators varying more rapidly than earlier ones") {
     val xValues = Range(1, 5)
     val yValues = Range(1, 3)
     val coordinates = for {
       x <- xValues
       y <- yValues} yield (x, y)
-    coordinates(4) should be(__, __)
+    coordinates(4) should be(3, 1)
   }
 }
