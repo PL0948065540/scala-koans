291fedf70a69611b170bec285bf18207b698d193
Meredith Godar <meredith.godar@gmail.com>
2016-06-29 21:42:50 -0500

    Completed AboutTypeTags

--- src/test/scala/org/functionalkoans/forscala/AboutTypeTags.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutTypeTags.scala
@@ -11,13 +11,13 @@ class AboutTypeTags extends KoanSuite {
          |   before it erased by the VM by using an implicit TypeTag argument.""") {
     def inspect[T](l: List[T])(implicit tt: TypeTag[T]) = tt.tpe.typeSymbol.name.decoded
     val list = 1 :: 2 :: 3 :: 4 :: 5 :: Nil
-    inspect(list) should be(__)
+    inspect(list) should be("Int")
   }
 
   koan("""TypeTags can also be """) {
     def inspect[T : TypeTag](l: List[T]) = typeOf[T].typeSymbol.name.decoded
     val list = 1 :: 2 :: 3 :: 4 :: 5 :: Nil
-    inspect(list) should be(__)
+    inspect(list) should be("Int")
   }
 
   koan("""TypeTags can be attached to classes. TypeTags have other meta-information about
@@ -26,6 +26,6 @@ class AboutTypeTags extends KoanSuite {
       def +(t: T) = "1 %s has been added".format(tt.tpe.typeSymbol.name.decoded) //Simple-name of the class erased
     }
     val candyBarrel = new Barrel[Candy]
-    (candyBarrel + new Candy) should be(__)
+    (candyBarrel + new Candy) should be("1 Candy has been added")
   }
 }
