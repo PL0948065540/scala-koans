5199b98c1997d04fd6baf51969e7afa11ef84172
Meredith Godar <meredith.godar@gmail.com>
2016-06-29 19:55:52 -0500

    Completed AboutImplicits

--- src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutImplicits.scala
@@ -16,8 +16,8 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
 
     implicit def thisMethodNameIsIrrelevant(value: Int) = new KoanIntWrapper(value)
 
-    19.isOdd should be(__)
-    20.isOdd should be(__)
+    19.isOdd should be(true)
+    20.isOdd should be(false)
   }
 
   koan("""Implicits rules can be imported into your scope with an import""") {
@@ -34,55 +34,52 @@ class AboutImplicits extends KoanSuite with ShouldMatchers {
 
     import MyPredef._
     //imported implicits come into effect within this scope
-    19.isOdd should be(__)
-    20.isOdd should be(__)
+    19.isOdd should be(true)
+    20.isOdd should be(false)
+    20.isEven should be(true)
   }
 
   koan("""Implicits can be used to automatically convert one type to another""") {
-
     import java.math.BigInteger
     implicit def Int2BigIntegerConvert(value: Int): BigInteger = new BigInteger(value.toString)
 
     def add(a: BigInteger, b: BigInteger) = a.add(b)
 
-    add(3, 6) should be(__)
+    add(3, 6) should be(new BigInteger("9"))
   }
 
-  koan("""Implicits can be used declare a value to be provided as a default as
-          |   long as an implicit value is set with in the scope.  These are
+  koan("""Implicits can be used to declare a value to be provided as a default as
+          |   long as an implicit value is set within the scope.  These are
           |   called implicit function parameters""") {
-
     def howMuchCanIMake_?(hours: Int)(implicit dollarsPerHour: BigDecimal) = dollarsPerHour * hours
 
     implicit var hourlyRate = BigDecimal(34.00)
 
-    howMuchCanIMake_?(30) should be(__)
+    howMuchCanIMake_?(30) should be(34.00 * 30)
 
     hourlyRate = BigDecimal(95.00)
-    howMuchCanIMake_?(95) should be(__)
+    howMuchCanIMake_?(95) should be(95.00 * 95)
   }
 
   koan("""Implicit Function Parameters can contain a list of implicits""") {
-
     def howMuchCanIMake_?(hours: Int)(implicit amount: BigDecimal, currencyName: String) =
       (amount * hours).toString() + " " + currencyName
 
     implicit var hourlyRate = BigDecimal(34.00)
     implicit val currencyName = "Dollars"
 
-    howMuchCanIMake_?(30) should be(__)
+    howMuchCanIMake_?(30) should be((34.00 * 30).toString() + " Dollars")
 
     hourlyRate = BigDecimal(95.00)
-    howMuchCanIMake_?(95) should be(__)
+    howMuchCanIMake_?(95) should be((95.00 * 95).toString() + " Dollars")
   }
 
   koan("""Default arguments though are preferred to Implicit Function Parameters""") {
-
     def howMuchCanIMake_?(hours: Int, amount: BigDecimal = 34, currencyName: String = "Dollars") =
       (amount * hours).toString() + " " + currencyName
 
-    howMuchCanIMake_?(30) should be(__)
+    howMuchCanIMake_?(30) should be((34 * 30).toString() + " Dollars")
 
-    howMuchCanIMake_?(95, 95) should be(__)
+    howMuchCanIMake_?(95, 95) should be((95 * 95).toString() + " Dollars")
   }
 }
